37c7fbcbdbde4156e069c5bb4d2f3adc5753c50d
Jose <jose.camilo@totvs.com.br>
2021-05-12 12:34:14 -0300

    remove configurações para HTTP

--- lib/main.dart
+++ lib/main.dart
@@ -5,18 +5,8 @@ import 'package:car_service_mobile/screens/search/search_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-class MyHttpOverrides extends HttpOverrides {
-  @override
-  HttpClient createHttpClient(SecurityContext context) {
-    return super.createHttpClient(context)
-      ..badCertificateCallback =
-          (X509Certificate cert, String host, int port) => true;
-  }
-}
-
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  HttpOverrides.global = new MyHttpOverrides();
   runApp(MyApp());
 }
 
